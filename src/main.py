3ab752173c23694185a1d76bef778a3b3260c402
JacksonDorschner <107083723+JacksonDorschner@users.noreply.github.com>
2024-01-26 11:43:12 -0800

    i need to go camping :(

--- src/main.py
+++ src/main.py
@@ -1,19 +1,37 @@
-from   func import kelvinConvert
+from schedule import repeat, every
+from helper import kelvinConvert
+from helper import get_time
 import datetime as dt
 import requests
+import schedule
+import asyncio
+import json
+import time
+import os
 
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=-22.6127615&lon=167.4874067&appid="
 API_KEY = open('api_key', 'r').read()
-
 url = BASE_URL + API_KEY
 response = requests.get(url).json()
+
+f = open('values.json')
+data = json.load(f)
 
 #Data Values:
 temp_kelvin  = response['main']['temp'    ]
 humidity     = response['main']['humidity']
-sunrise_time = dt.datetime.utcfromtimestamp(response['sys' ]['sunrise' ] + response['timezone'])
-sunset_time  = dt.datetime.utcfromtimestamp(response['sys' ]['sunset'  ] + response['timezone'])
+sunrise_time = response['sys' ]['sunrise' ] + response['timezone']
+sunset_time  = response['sys' ]['sunset'  ] + response['timezone']
 
 temp_celcius, temp_fahrenheit = kelvinConvert(temp_kelvin)
+now = str(dt.datetime.now().hour)
 
-print(f'Temp: {temp_fahrenheit}F, {temp_celcius}C \n Humidity: {humidity}% \n Sunrise: {sunrise_time} \n Sunset: {sunset_time}')
+#@repeat(every().minute.at(':30')) Temp: %(temp_fahrenheit)F, %(temp_celcius)C \n Humidity: %(humidity) \n Sunrise: %(sunrise_time) \n Sunset: %(sunset_time)
+def fetch() -> str:
+    print(f'Temp: {temp_fahrenheit}F, {temp_celcius}C \n Humidity: {humidity}% \n Sunrise: {sunrise_time} \n Sunset: {sunset_time}')
+    
+    
+'''
+while True:
+    schedule.run_pending()
+    time.sleep(1)'''
