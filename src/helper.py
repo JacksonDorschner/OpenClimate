8f164e78ee8653bd3ee7b48e9383be7e7331a24f
Jackson Dorschner <107083723+JacksonDorschner@users.noreply.github.com>
2024-01-29 07:50:23 +0000

    working on write + api_fetch updates

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,7 @@
+import requests
 import json
+
+utc_Offset = 19
 
 def kelvinConvert(kelvin: float) -> tuple:
     celcius = kelvin - 273.15
@@ -6,7 +9,7 @@ def kelvinConvert(kelvin: float) -> tuple:
     return celcius, fahrenheit
 
 class storage:
-    def read(now):
+    def read(now: int) -> tuple:
         """returns tuple for setting current values for the system to maintain
 
         Args:
@@ -23,10 +26,30 @@ class storage:
         f.close()
         return env_control
     
-    def write(now):
+    def write(now: int, self):
         f = open('values.json')
         data = json.load(f)
+        futureTime = int(now) + utc_Offset
+        if futureTime > 24:
+            futureTime = futureTime - 24
+        self.api_fetch()
+
+        
         f.close()
         
-    def api_fetch() -> str:
-            return
+    def api_fetch() -> tuple:
+        #API
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=-22.6127615&lon=167.4874067&appid="
+        API_KEY = open('api_key', 'r').read()
+        url = BASE_URL + API_KEY
+        response = requests.get(url).json()
+
+        #Data Values:
+        temp_kelvin  = response['main']['temp'    ]
+        humidity     = response['main']['humidity']
+        sunrise_time = response['sys' ]['sunrise' ] + response['timezone']
+        sunset_time  = response['sys' ]['sunset'  ] + response['timezone']
+
+        #helping bits
+        temp_celcius, temp_fahrenheit = kelvinConvert(temp_kelvin)
+        return temp_fahrenheit, humidity
--- src/main.py
+++ src/main.py
@@ -1,30 +1,17 @@
 from helper   import kelvinConvert, storage
 from schedule import repeat, every
 import datetime as dt
-import requests
 import json
 import time
 
-#API
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=-22.6127615&lon=167.4874067&appid="
-API_KEY = open('api_key', 'r').read()
-url = BASE_URL + API_KEY
-response = requests.get(url).json()
-
-#Data Values:
-temp_kelvin  = response['main']['temp'    ]
-humidity     = response['main']['humidity']
-sunrise_time = response['sys' ]['sunrise' ] + response['timezone']
-sunset_time  = response['sys' ]['sunset'  ] + response['timezone']
-
 #helping bits
-temp_celcius, temp_fahrenheit = kelvinConvert(temp_kelvin)
 now = str(dt.datetime.now().hour)
 
 if __name__ == '__main__':
     def init():
         return
-    storage.read(now)
+    #storage.read(now)
+    storage.write(now)
     
 '''
 @repeat(every().minute.at(':30'))
