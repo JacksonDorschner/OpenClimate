571f678b7c4f9e2174dcda1560d21a0c59cdd8d0
JacksonDorschner <107083723+JacksonDorschner@users.noreply.github.com>
2024-01-28 20:03:57 -0800

    yummy yummy

--- src/helper.py
+++ src/helper.py
@@ -1,4 +1,30 @@
+from main import temp_celcius, temp_fahrenheit, humidity, sunrise_time, sunset_time, json
+import json
+
 def kelvinConvert(kelvin: float) -> tuple:
     celcius = kelvin - 273.15
     fahrenheit = celcius * (9/5) + 32
     return celcius, fahrenheit
+
+class storage:
+    def read(now) -> tuple:
+        """returns tuple for setting current values for the system to maintain
+
+        Args:
+            now (int): current hour of the day (24h time)
+
+        Returns:
+            tuple: all the data for current conditions
+        """
+        f = open('values.json')
+        data = json.load(f)
+        
+        for i in data['']:
+            return
+    
+    def write(now):
+        f = open('values.json')
+        data = json.load(f)
+        
+    def api_fetch() -> str:
+        print(f'Temp: {temp_fahrenheit}F, {temp_celcius}C \n Humidity: {humidity}% \n Sunrise: {sunrise_time} \n Sunset: {sunset_time}')
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from schedule import repeat, every
 from helper import kelvinConvert
-from helper import get_time
 import datetime as dt
 import requests
 import schedule
@@ -9,13 +8,18 @@ import json
 import time
 import os
 
+#API
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=-22.6127615&lon=167.4874067&appid="
 API_KEY = open('api_key', 'r').read()
 url = BASE_URL + API_KEY
 response = requests.get(url).json()
 
-f = open('values.json')
-data = json.load(f)
+#storage
+current_parrams = ''
+
+#helping bits
+temp_celcius, temp_fahrenheit = kelvinConvert(temp_kelvin)
+now = str(dt.datetime.now().hour)
 
 #Data Values:
 temp_kelvin  = response['main']['temp'    ]
@@ -23,15 +27,13 @@ humidity     = response['main']['humidity']
 sunrise_time = response['sys' ]['sunrise' ] + response['timezone']
 sunset_time  = response['sys' ]['sunset'  ] + response['timezone']
 
-temp_celcius, temp_fahrenheit = kelvinConvert(temp_kelvin)
-now = str(dt.datetime.now().hour)
+if __name__ == '__main__':
+    def init():
+        return
 
-#@repeat(every().minute.at(':30')) Temp: %(temp_fahrenheit)F, %(temp_celcius)C \n Humidity: %(humidity) \n Sunrise: %(sunrise_time) \n Sunset: %(sunset_time)
-def fetch() -> str:
-    print(f'Temp: {temp_fahrenheit}F, {temp_celcius}C \n Humidity: {humidity}% \n Sunrise: {sunrise_time} \n Sunset: {sunset_time}')
-    
     
 '''
+@repeat(every().minute.at(':30')) Temp: %(temp_fahrenheit)F, %(temp_celcius)C \n Humidity: %(humidity) \n Sunrise: %(sunrise_time) \n Sunset: %(sunset_time)
 while True:
     schedule.run_pending()
     time.sleep(1)'''
